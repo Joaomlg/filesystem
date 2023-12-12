7ac598ee7430b0caf8877b554a0c1390936807f5
joaomlg <joaomarcos1999jm@gmail.com>
2023-12-11 21:14:38 -0300

    feature: test9 created to check write existing file

--- grade.sh
+++ grade.sh
@@ -4,7 +4,7 @@ set -u
 # DCC605F5: Filesystem implementation programming assignment
 # Autograding script
 
-total=8
+total=9
 ecnt=0
 
 if ! tests/test1.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
@@ -15,6 +15,7 @@ if ! tests/test5.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test6.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test7.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 if ! tests/test8.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
+if ! tests/test9.sh ; then ecnt=$(( $ecnt + 1 )) ; fi
 
 echo "your code passes $(( $total - $ecnt )) of $total tests"
 rm -f fs.o
